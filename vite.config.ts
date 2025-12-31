ab0e7f898bc4628d9f4c0856c9068dbe80cbf49f
Kats-N <exxn.ash@gmail.com>
2025-12-31 17:15:16 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ export default defineConfig({
   plugins: [react()],
   base: './',
   define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+    'process.env.FB_API_KEY': JSON.stringify(process.env.FB_API_KEY),
+    'process.env.FB_AUTH_DOMAIN': JSON.stringify(process.env.FB_AUTH_DOMAIN),
+    'process.env.FB_PROJECT_ID': JSON.stringify(process.env.FB_PROJECT_ID),
+    'process.env.FB_STORAGE_BUCKET': JSON.stringify(process.env.FB_STORAGE_BUCKET),
+    'process.env.FB_MESSAGING_SENDER_ID': JSON.stringify(process.env.FB_MESSAGING_SENDER_ID),
+    'process.env.FB_APP_ID': JSON.stringify(process.env.FB_APP_ID),
   }
 });
